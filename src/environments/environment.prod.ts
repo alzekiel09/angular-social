bd180c40c4759d4a75e96897577c835b149ddcc3
alzekiel09 <aixonairashii@gmail.com>
2018-03-08 02:22:59 +0800

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAf7_x0guazlbR9pjqfkUhxwfOr2wJmelw",
+		authDomain: "firestore-crud-of-users-9a145.firebaseapp.com",
+		databaseURL: "https://firestore-crud-of-users-9a145.firebaseio.com",
+		projectId: "firestore-crud-of-users-9a145",
+		storageBucket: "firestore-crud-of-users-9a145.appspot.com",
+		messagingSenderId: "697289795538"
+    }
+};
 
